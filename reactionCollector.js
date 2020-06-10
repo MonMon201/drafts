b57e61e244c2324a92a205f01320f725d3dc5429
MonMon201 <danyavasilinenko213@gmail.com>
2020-06-10 21:29:56 +0300

    feat: testing reaction event and create ReactionCollector

--- reactionCollector.js
+++ reactionCollector.js
@@ -4,9 +4,24 @@ const Discord = require('discord.js');
 require('dotenv').config()
 
 const client = new Discord.Client();
- 
+
+const collectorCreator = (message, collector) =>{
+    collector = new Discord.ReactionCollector(message, () =>{});
+    return collector;
+}
+
+let collector = null;
+
 client.on('message', message => {
     console.log(message.content);
+    if(!collector)
+    collector = collectorCreator(message, collector) 
 });
+
+client.on('messageReactionAdd', () => {
+    console.log('reaction!');    
+});
+
+
 
 client.login(process.env.TOKEN);
