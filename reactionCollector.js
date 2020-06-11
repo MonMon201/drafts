c923781904a714d62a794ed18726022a561340b4
MonMon201 <danyavasilinenko213@gmail.com>
2020-06-11 12:21:07 +0300

    feat: change structure of collector; Final commit

--- reactionCollector.js
+++ reactionCollector.js
@@ -5,23 +5,27 @@ require('dotenv').config()
 
 const client = new Discord.Client();
 
-const collectorCreator = (message, collector) =>{
-    collector = new Discord.ReactionCollector(message, () =>{});
-    return collector;
-}
+const filter = (reaction) => {
+	return reaction; //There we can set filter conditions
+};
 
 let collector = null;
 
 client.on('message', message => {
     console.log(message.content);
-    if(!collector)
-    collector = collectorCreator(message, collector) 
+    collector = message.createReactionCollector(filter, { time: 15000 }); 
+    //  createReaction...(message, filter, option) - 1-2 are required 
+    //  3 - is optional (there is a timer in this case)
 });
 
 client.on('messageReactionAdd', () => {
-    console.log('reaction!');    
+    console.log('reaction!');
+    collector.on('collect', (reaction) => {
+        console.log(`Collected ${reaction.emoji.name}`);
+    });
+    collector.on('end', collected => {  //this happens when timer ends
+        console.log(`Collected ${collected.size} items`);
+    });    
 });
-
-
 
 client.login(process.env.TOKEN);
