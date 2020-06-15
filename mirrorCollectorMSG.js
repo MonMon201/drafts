81227e6eb1f800e839f228cc4c6678ef51d144fd
MonMon201 <danyavasilinenko213@gmail.com>
2020-06-15 18:24:54 +0300

    fix: fix overflow in collected messages

--- mirrorCollectorMSG.js
+++ mirrorCollectorMSG.js
@@ -8,6 +8,7 @@ const handler = require("./src/handler.js");
 const client = new Discord.Client();
 
 client.on('message', message => {
+    if(message.author.username!=="Danias-Test-Bot")
     handler.wrapper(message, client);
 });
 
--- src/function/collector.js
+++ src/function/collector.js
@@ -9,9 +9,15 @@ const stopCollect = (messageWrap) => {
 const showCollected = (messageWrap) => {
     const channelID = messageWrap.message.channel.id;
     const channel = messageWrap.client.channels.cache.get(channelID);
-    for(let i = 0; i < messageWrap.collectedMessages.length; i++){
-        const content = messageWrap.collectedMessages[i].content;
-        channel.send(content);
+    if(messageWrap.collectedMessages.length){
+        for(let i = 0; i < messageWrap.collectedMessages.length; i++){
+            const content = messageWrap.collectedMessages[i].content;
+            channel.send(content);
+        }
+        messageWrap.collectedMessages = [];
+    }
+    else{
+        channel.send('There is nothing collected!');
     }
 };
 
