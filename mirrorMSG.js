51d6ecdda0fbb4173358956eb3c34514edd5eef6
MonMon201 <danyavasilinenko213@gmail.com>
2020-06-15 14:20:45 +0300

    feat: add base discord bot structure

---
+++ mirrorMSG.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Discord = require('discord.js');
+require('dotenv').config()
+
+const client = new Discord.Client();
+ 
+client.on('message', message => {
+    if(message.content === "ping"){
+        console.log(message);
+        // const channelID = message.channel.id;
+        // const channel = client.channels.cache.get(channelID);
+        // channel.send("pong");
+    }
+});
+
+client.login(process.env.TOKEN);
