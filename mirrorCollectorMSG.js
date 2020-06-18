674d812758d45b85706208b0229e453ae4583805
MonMon201 <danyavasilinenko213@gmail.com>
2020-06-18 11:51:28 +0300

    chore: switching to other branch due to impossibility to continue working without missing details

--- mirrorCollectorMSG.js
+++ mirrorCollectorMSG.js
@@ -1,13 +1,16 @@
 'use strict';
 require('dotenv').config();
 const Discord = require('discord.js');
-const handler = require('./src/handler.js');
 
 const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log('yay!');
+});
+
 client.on('message', message => {
     if(message.author.username!=="Danias-Test-Bot")
-    handler.wrapper(message, client);
+    console.log('yay!');
 });
 
 client.login(process.env.TOKEN);
--- src/handler.js
+++ src/handler.js
@@ -7,13 +7,137 @@ const {
   showCollected,
 } = require('./function/collector.js');
 
+class Distributor{
+  constructor(defaultFn, switchObj){
+      this.defaultFn = defaultFn;
+      this.switchObj = switchObj;
+      this.funcColl = [];
+  }
+  
+  static create(defaultFn){
+      return new Distributor(defaultFn);
+  }
+
+  add(key, value){
+      this.funcColl.push({key : key, value : value});
+      return this;
+  }
+
+  controller(wrappedMessage){
+      for(let i = 0; i < this.funcColl.length; i++){
+          if(this.funcColl[i].key === wrappedMessage.message.content){
+              return this.funcColl[i].value(wrappedMessage);
+          }
+      }
+      return this.defaultFn(wrappedMessage);
+  }
+
+}
+
+class Handler{
+  constructor(defaultDistrubutor){
+      this.distributors = [];
+      this.defaultDistrubutor = defaultDistrubutor;
+  }
+
+  static create(defaultDistrubutor){
+      return new Handler(defaultDistrubutor);
+  }
+
+  add(key, value){// key === flag of the distributor, will be the system of connection keys-distributors
+      this.distributors.push({key : key, value : value});
+      return this;
+  }
+
+  controller(wrappedMessage){
+      const content = wrappedMessage.message.content;
+      for(let key in wrappedMessage.flagStorage){
+          if(wrappedMessage.flagStorage[key]) // if some flag is up this will happen
+              
+              for(let i = 0; i < this.distributors.length; i++){
+                  if(this.distributors[i].key===key){
+                      return this.distributors[i].value.controller(wrappedMessage);
+                  }
+              }
+
+      }
+      return this.defaultDistrubutor.controller(wrappedMessage);
+  }
+}
+
+class Wrapper{
+  constructor(){
+    this.client = null;
+    this.message = null;
+    this.storedFlags = [];
+    this.storedMessages = [];
+  }
+
+  static create(){
+    return new Wrapper();
+  }
+
+  setClient(client){
+    this.client = client;
+  }
+
+  setMessage(message){
+    this.message = message;
+  }
+
+  setFlag(key, value){
+    this.storedFlags.push({key : key, value : value});
+  }
+
+  addMessages(message){
+    this.storedMessages.push(message);
+  }
+
+  getClient(){
+    return this.client;
+  }
+
+  getMessage(){
+    return this.message;
+  }
+
+  getFlag(key){
+    for(let i = 0; i < this.storedFlags.length; i++){
+      if(this.storedFlags[i].key === key){
+        return this.storedFlags[i].value;
+      }
+    }
+  }
+
+  getMessages(){
+    return this.storedMessages;
+  }
+
+  getWrappedMessage(){
+    const wrappedMessage = {
+      message : this.message,
+      client : this.client,
+      storedFlags : this.storedFlags,
+      storedMessages : this.storedMessages,
+    };
+    return wrappedMessage;
+  }
+
+}
+
+class Controller{
+  constructor(){
+
+  }
+
+  init(client){}
+}
+
+
+
+/*
 const messageWrap = {
-  flagsCollection: {
-    collectorFlag: false,
-  },
-  collectedMessages: new Messages(),
-  client: null,
-  message: null,
+
 };
 
 const collecterDistributor = messageWrap => {
@@ -47,7 +171,7 @@ const flagCheck = messageWrap => {
   return defaultDistributor(messageWrap);
 };
 
-const wrapper = (message, client) => {
+const wrap = (message, client) => {
   console.log(message.content);
   messageWrap.message = message;
   messageWrap.client = client;
@@ -55,5 +179,6 @@ const wrapper = (message, client) => {
 };
 
 module.exports = {
-  wrapper,
+  wrap,
 };
+*/
