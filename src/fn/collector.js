b482b9c75b4c992759f2a45006cb07448c145c8f
MonMon201 <danyavasilinenko213@gmail.com>
2020-06-18 16:06:44 +0300

    feat: add collector functions

---
+++ src/fn/collector.js
@@ -0,0 +1,24 @@
+const startCollect = (messageWrap) => {
+    messageWrap.flagsCollection.collectorFlag = true;
+};
+
+const stopCollect = (messageWrap) => {
+    messageWrap.flagsCollection.collectorFlag = false;
+};
+
+const showCollected = (messageWrap) => {
+    const channelID = messageWrap.message.channel.id;
+	const channel = messageWrap.client.channels.cache.get(channelID);
+	const collectedMessages = messageWrap.collectedMessages.toArray()
+	console.log(collectedMessages)
+    for(let i = 0; i < collectedMessages.length; i++){
+        const content = collectedMessages[i].content;
+        channel.send(content);
+    }
+};
+
+module.exports = {
+    startCollect,
+    stopCollect,
+    showCollected,
+};
