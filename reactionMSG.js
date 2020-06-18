a351361c791b038b618e99ee175a54d2ecb3a14f
MonMon201 <danyavasilinenko213@gmail.com>
2020-06-18 17:23:28 +0300

    Chore: remove comments

--- reactionMSG.js
+++ reactionMSG.js
@@ -18,31 +18,3 @@ client.on('ready', () => {
 });
 
 client.login(process.env.TOKEN);
-/*
-const channelID = message.channel.id;
-const channel = client.channels.cache.get(channelID);
-channel.send("pong");
-
-const filter = (reaction) => {
-	return reaction; //There we can set filter conditions
-};
-
-let collector = null;
-
-client.on('message', message => {
-    console.log(message.content);
-    collector = message.createReactionCollector(filter, { time: 15000 }); 
-    //  createReaction...(message, filter, option) - 1-2 are required 
-    //  3 - is optional (there is a timer in this case)
-});
-
-client.on('messageReactionAdd', () => {
-    console.log('reaction!');
-    collector.on('collect', (reaction) => {
-        console.log(`Collected ${reaction.emoji.name}`);
-    });
-    collector.on('end', collected => {  //this happens when timer ends
-        console.log(`Collected ${collected.size} items`);
-    });    
-});
-*/
