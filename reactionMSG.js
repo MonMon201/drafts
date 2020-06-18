acdd8ef65aa32e2c43690bb1519bc6ad76a47a28
MonMon201 <danyavasilinenko213@gmail.com>
2020-06-18 14:32:54 +0300

    feat: add message marking with stiker

--- reactionMSG.js
+++ reactionMSG.js
@@ -1,14 +1,41 @@
 'use strict';
 
 const Discord = require('discord.js');
-const reactionMSG = require('./src/reactionMSG');
 require('dotenv').config()
 const {createReactionMSG} = require('./src/createMSG.js');
 
 const client = new Discord.Client();
  
 client.on('message', message => {
-    console.log(message.content);
+    createReactionMSG(message);
 });
 
 client.login(process.env.TOKEN);
+/*
+const channelID = message.channel.id;
+const channel = client.channels.cache.get(channelID);
+channel.send("pong");
+
+const filter = (reaction) => {
+	return reaction; //There we can set filter conditions
+};
+
+let collector = null;
+
+client.on('message', message => {
+    console.log(message.content);
+    collector = message.createReactionCollector(filter, { time: 15000 }); 
+    //  createReaction...(message, filter, option) - 1-2 are required 
+    //  3 - is optional (there is a timer in this case)
+});
+
+client.on('messageReactionAdd', () => {
+    console.log('reaction!');
+    collector.on('collect', (reaction) => {
+        console.log(`Collected ${reaction.emoji.name}`);
+    });
+    collector.on('end', collected => {  //this happens when timer ends
+        console.log(`Collected ${collected.size} items`);
+    });    
+});
+*/
--- src/createMSG.js
+++ src/createMSG.js
@@ -1,7 +1,17 @@
 'use strict'
 
-const createReactionMessage = (wrappedMessage) => {
-
+const createReactionMessage = (message) => {
+    if(message.author.username !== 'Danias-Test-Bot'){
+        if(message.content === 'ping'){
+            console.log(message.content);
+            const channelID = message.channel.id;
+            const channel = client.channels.cache.get(channelID);
+            channel.send("pong");
+        }
+    }
+    else{
+        message.react('😄');
+    }
 };
 
 module.exports = {
