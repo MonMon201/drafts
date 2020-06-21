a82fee235326ebac1e775acc9934b5425f993941
MonMon201 <danyavasilinenko213@gmail.com>
2020-06-21 19:16:17 +0300

    Chore: halfmake new feat

--- reactionCounter.js
+++ reactionCounter.js
@@ -1,12 +1,34 @@
 'use strict';
 
 const Discord = require('discord.js');
+const { KeyObject } = require('crypto');
 require('dotenv').config()
 
 const client = new Discord.Client();
- 
+
+const filter = (reaction, user) => {
+    return reaction.emoji.name === '➕';
+    // ➕
+};
+
+let collector = null;
+const reactionCounter = [];
+
+client.on('ready', () => {
+    console.log('ready');
+});
+
 client.on('message', message => {
-    console.log(message.content);
+    collector = message.createReactionCollector(filter, { time: 60000 }); 
+    console.log(message.id);
+    //  createReaction...(message, filter, option) - 1-2 are required 
+    //  3 - is optional (there is a timer in this case)
+});
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+    // console.log('reaction!');
+    console.log(messageReaction._emoji.name);
+    console.log(user.tag);
 });
 
 client.login(process.env.TOKEN);
